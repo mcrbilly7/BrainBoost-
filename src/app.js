7f1a26d545ac0d8f6c42ee30a0e51dbcb06c41ee
mcrbilly7 <nossonkosowsky@gmail.com>
2025-06-06 19:15:33 -0500

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Game from './games/QuickMathChallenge';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/game/1" element={<Game />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
